dd6b2d776bd24e5c342d3d82942b5dab7745135f
pintu jha <pintukumar0501@gmail.com>
2021-04-12 19:44:35 +0530

    first commit

---
+++ src/components/Reservation/ViewReservation.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import HomeIcon from '@material-ui/icons/Home';
+import Link from '@material-ui/core/Link';
+export default class ViewReservation extends React.Component{
+    render(){
+        return(
+            <>
+            {/* =============BREAD CRUMBS SECTION CODE START==================== */}
+  <Breadcrumbs className="bread-link-sec" aria-label="breadcrumb">
+             <Link color="inherit" href="/NewReservation">New Reservation</Link>
+             <Link color="inherit" href="/ViewReservation">View Reservation Details</Link>
+             <Link color="inherit" href="/CancelList">Cancel List</Link>
+             <Link color="inherit" href="/TravelAgentThirdParty">Travel Agent / Third Party Side</Link>
+          </Breadcrumbs>
+         
+ 
+{/* =============BREAD CRUMBS SECTION CODE START==================== */}
+                <h1>View Reservation</h1>
+            </>
+        )
+    }
+}
